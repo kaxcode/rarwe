3bc9b65083a5e6aeb54dd36c9548f3b29ea1aa46
Kenia Valladarez <19161242+kaxcode@users.noreply.github.com>
2020-03-31 20:14:37 -0400

    Create controller and tempale for bands

---
+++ app/controllers/bands.js
@@ -0,0 +1,19 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default Controller.extend({
+  isAddingBand: false,
+  newBandName: '',
+
+  addBand: action(() => {
+    this.set('isAddingBand', true)
+  }),
+
+  cancelAddBand: action(function() {
+    this.set('isAddingBand', false)
+  }),
+
+  saveBand: action(function() {
+    // Create a new band
+  })
+});
