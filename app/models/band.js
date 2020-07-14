94bc8ec36716d8e6729e6dad94b3af54d8db770d
kenia <keniavalladarez@gmail.com>
2020-07-14 13:42:56 -0400

    Using JSON:API to return and save bands

--- app/models/band.js
+++ app/models/band.js
@@ -4,9 +4,10 @@ export default class Band {
   @tracked name;
   @tracked songs;
 
-  constructor({ name, slug, songs }) {
+  constructor({ id, name, songs }, relationships={}) {
+    this.id = id;
     this.name = name;
-    this.slug = slug;
     this.songs = songs ?? [];
+    this.relationships = relationships;
   }
 }
